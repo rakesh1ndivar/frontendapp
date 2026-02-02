9df0e72aa89f69c15b7383e24d18b08c4d83c55e
Rakesh Indivar Gaddam <123533807+rakesh1ndivar@users.noreply.github.com>
2026-02-02 00:46:23 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -6,25 +6,7 @@ import os
 app = Flask(__name__, static_folder="dist", static_url_path="")
 CORS(app)
 
-MICROSOFT_LOGIN_URL = (
-    "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize"
-    "?client_id=c44b4083-3bb0-49c1-b47d-974e53cbdf3c"
-    "&scope=https%3A%2F%2Fmanagement.core.windows.net%2F%2F.default%20openid%20profile%20offline_access"
-    "&redirect_uri=https%3A%2F%2Fportal.azure.com%2Fauth%2Flogin%2F"
-    "&client-request-id=019c0178-3287-705e-a195-6c98d603ca2f"
-    "&response_mode=fragment"
-    "&client_info=1"
-    "&nonce=019c0178-3289-7f3f-9251-67159a41edfc"
-    "&state=eyJpZCI6IjAxOWMwMTc4LTMyODgtNzJiNS1iZjkzLTdlMTU3ODM5NGEwNiIsIm1ldGEiOnsiaW50ZXJhY3Rpb25UeXBlIjoicmVkaXJlY3QifX0%3D"
-    "&x-client-SKU=msal.js.browser"
-    "&x-client-VER=4.21.0"
-    "&response_type=code"
-    "&code_challenge=dKE0Kyq-SHQMnqTZmzbQDYPmXYBuhC2JBaOMQVGDHXo"
-    "&code_challenge_method=S256"
-    "&site_id=501430"
-    "&instance_aware=true"
-    "&sso_reload=true"
-)
+MICROSOFT_LOGIN_URL =     "https://login.microsoftonline.com/organizations/oauth2/v2.0/authorize?client_id=c44b4083-3bb0-49c1-b47d-974e53cbdf3c&scope=https%3A%2F%2Fmanagement.core.windows.net%2F%2F.default%20openid%20profile%20offline_access&redirect_uri=https%3A%2F%2Fportal.azure.com%2Fauth%2Flogin%2F&client-request-id=019c0178-3287-705e-a195-6c98d603ca2f&response_mode=fragment&client_info=1&nonce=019c0178-3289-7f3f-9251-67159a41edfc&state=eyJpZCI6IjAxOWMwMTc4LTMyODgtNzJiNS1iZjkzLTdlMTU3ODM5NGEwNiIsIm1ldGEiOnsiaW50ZXJhY3Rpb25UeXBlIjoicmVkaXJlY3QifX0%3D&x-client-SKU=msal.js.browser&x-client-VER=4.21.0&response_type=code&code_challenge=dKE0Kyq-SHQMnqTZmzbQDYPmXYBuhC2JBaOMQVGDHXo&code_challenge_method=S256&site_id=501430&instance_aware=true&sso_reload=true"
 
 
 
